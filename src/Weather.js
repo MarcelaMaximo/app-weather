dfaa742299b650a7cab5dcf5c7cb89a8fdab09b2
MarcelaMaximo <78973749+MarcelaMaximo@users.noreply.github.com>
2021-05-15 21:38:53 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -68,7 +68,7 @@ export default  function Weather(props) {
     );
   } else {
     const apiKey = "20d1aee4a985cfd10c02f2da340417b7";
-    let apiUrl =`http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);  
   
     return "Loading..."
