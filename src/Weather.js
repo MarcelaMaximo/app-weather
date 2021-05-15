a5cb5cfaade3efecaf8e64584c3422168bcce518
MarcelaMaximo <78973749+MarcelaMaximo@users.noreply.github.com>
2021-05-15 19:54:41 +0100

    adding API

--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import './Weather.css';
 
 export default  function Weather() {
-  return (
-    <div className="Weather">
-      <div className="city">
-        <form>
-      <div className="row">
-        <div className="col-9"> 
-             <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-              />
+  const [WeatherData, setWeatherData]= useState({ ready: false});
+  function handleResponse (response) {
+    console.log(response.data);
+    setWeatherData({
+      ready:true,
+      date: "13:03 - 3 Feb (Thursday)", 
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed, 
+      city: response.data.name,
+      country: response.data.sys.country,
+      min: response.data.main.temp_min,
+      max: response.data.main.temp_max,
+      humidity: response.data.main.humidity,
+      icon: "http://openweathermap.org/img/wn/10d@2x.png"
+
+    });
+  }
+
+  if (WeatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="city">
+          <form>
+        <div className="row">
+          <div className="col-9"> 
+              <input
+                  type="search"
+                  placeholder="Enter a city..."
+                  className="form-control"
+                  autoFocus="on"
+                />
+          </div>
+          <div className="col-3"> 
+          <input type="submit" value="Search" className="btn btn-outline-primary w-100"/>        
+          </div>
         </div>
-        <div className="col-3"> 
-        <input type="submit" value="Search" className="btn btn-outline-primary w-100"/>        
+      </form>
+        <h2 className="day"> {WeatherData.date}</h2>
+          <h1>
+            {WeatherData.city}, <span className="country">{WeatherData.country}</span>{" "}
+          </h1>
+          
         </div>
-      </div>
-    </form>
-      <h2 className="day">13:03 - 3 Feb (Thursday)</h2>
-        <h1>
-          Lisbon, <span className="country">PT</span>{" "}
-        </h1>
-        
-      </div>
-      <div className="temperature"> 
-        <span className="temp"> 20</span> 
-        <span className="unit">°C | °F</span>        
-        <div className="img"> <img
-            src="http://openweathermap.org/img/wn/10d@2x.png "
-            className="emoji"
-          />
+        <div className="temperature"> 
+          <span className="temp"> {Math.round(WeatherData.temperature)}</span> 
+          <span className="unit">°C | °F</span>        
+          <div className="img"> <img
+              src={WeatherData.icon}
+              className="emoji"
+            />
+            </div>
+        </div>
+          <div className="row mt-3"> 
+            <div className="col-2">  </div>
+            <div className="col-2">  <strong>Max</strong><div  className="description">{Math.round(WeatherData.max)}°C</div></div>
+            <div className="col-2"> <strong>Min</strong><div className="description">{Math.round(WeatherData.min)}°C</div></div>
+            <div className="col-2"> <strong>Wind</strong><div className="description">{Math.round(WeatherData.wind)}km/h</div></div>
+            <div className="col-2"> <strong>Humidity</strong><div className="description">{Math.round(WeatherData.humidity)}%</div></div>
+            <div className="col-2"> </div>
           </div>
       </div>
-        <div className="row mt-3"> 
-          <div className="col-2">  </div>
-          <div className="col-2">  <strong>Max</strong><div  className="description">9°C</div></div>
-          <div className="col-2"> <strong>Min</strong><div className="description">9°C</div></div>
-          <div className="col-2"> <strong>Wind</strong><div className="description">3km/h</div></div>
-          <div className="col-2"> <strong>Humidity</strong><div className="description">45%</div></div>
-          <div className="col-2"> </div>
-        </div>
-  </div>
-  );
+    );
+  } else {
+    const apiKey = "fb4290424ee04cbcc7671069110859c2";
+    let city ="Lisbon";
+    let apiUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);  
+  
+    return "Loading..."
 }
+  }
+
+   
 
