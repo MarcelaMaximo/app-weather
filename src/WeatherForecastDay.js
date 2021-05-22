f7f6b85fd3805b93ebdfaafaf101341be8abd13c
MarcelaMaximo <78973749+MarcelaMaximo@users.noreply.github.com>
2021-05-22 22:51:35 +0100

    add card.text weatherforecast day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-import { Card } from 'react-bootstrap';
+import { Card, CardColumns } from 'react-bootstrap';
 
 export default function WeatherForecastDay (props){
 
@@ -24,14 +24,18 @@ export default function WeatherForecastDay (props){
 
     return(
       <div className="WeatherForecastDay">
-        <Card>
-          <div className="day"> {day()} </div>
-          <WeatherIcon code={props.data.weather[0].icon} size={50}/>
-          <div className="temperature"> 
-          <span className="temperature-max">{MaxTemp()}</span>
-          <span className="temperature-min">{MinTemp()}</span>
-          </div>
-        </Card>
+        <CardColumns> 
+          <Card>
+            <Card.Text>
+            <div className="day"> {day()} </div>
+            <WeatherIcon code={props.data.weather[0].icon} size={50}/>
+            <div className="temperature"> 
+            <span className="temperature-max">{MaxTemp()}</span>
+            <span className="temperature-min">{MinTemp()}</span>
+            </div>
+            </Card.Text>
+          </Card>
+        </CardColumns>
       </div> 
     );
   }
